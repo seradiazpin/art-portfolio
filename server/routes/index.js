6ef75b1fd89b122a3ac81eddd326b795f81d4ad4
seradiazpin <seradiazpin@unal.edu.co>
2017-01-24 12:56:06 -0500

    Routes of login

---
+++ server/routes/index.js
@@ -0,0 +1,76 @@
+/**
+ * Created by sergiodiazpinilla on 11/01/17.
+ */
+
+const routes = require('express').Router();
+
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+//const profile = require('./perfil');
+
+
+// =====================================
+// HOME PAGE (with login links) ========
+// =====================================
+routes.get('/', function(req, res) {
+    res.render('index.html'); // load the index.ejs file
+});
+
+// =====================================
+// LOGIN ===============================
+// =====================================
+// show the login form
+routes.get('/login', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login.html', { message: req.flash('loginMessage') });
+});
+
+// process the login form
+// app.post('/login', do all our passport stuff here);
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+routes.get('/signup', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+});
+
+// process the signup form
+// app.post('/signup', do all our passport stuff here);
+
+// =====================================
+// PROFILE SECTION =====================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+routes.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+    });
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+routes.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+module.exports = routes;
